ec4aa637af97b2b2479c6650f93af8c27017d286
Guilherme Figueira <guibf2004@gmail.com>
2023-03-08 13:39:19 -0300

    react hook form e react number format

--- src/components/Percentage.tsx
+++
@@ -1,3 +0,0 @@
-"bg-cyan-very_light text-cyan-dark focus-within:ring-2 focus-within:ring-cyan-strong ";
-"bg-cyan-strong text-cyan-very_dark";
-"bg-cyan-very_dark text-white hover:bg-[#9fe8df] hover:text-cyan-very_dark";
